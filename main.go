45b3e551334feaf0049e8d9f83635c416331a5da
Engineerbabu777 <awaismumtaz0099@gmail.com>
2024-05-24 09:36:36 +0500

    Add: created a basic user type struct or model

--- main.go
+++ main.go
@@ -2,12 +2,11 @@ package main
 
 import "github.com/gofiber/fiber"
 
+func main() {
+	app := fiber.New()
 
-func main(){
-	app := fiber.New();
-
-	app.Get("/foo", func (c *fiber.Ctx){
-		 c.JSON(map[string]string{"msg":"ALl Ok"})
+	app.Get("/foo", func(c *fiber.Ctx) {
+		c.JSON(map[string]string{"msg": "ALl Ok"})
 	})
 
 	app.Listen(":5000")
